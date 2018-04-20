a346072d43b86a05f82920ee869a9f7d8ef41009
Florian Pondepeyre <fpondepeyre@lefigaro.fr>
2018-04-20 17:07:22 +0200

    clean

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -17,8 +17,6 @@ class TaskType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            // Configure your form options here
-        ]);
+        $resolver->setDefaults([]);
     }
 }
